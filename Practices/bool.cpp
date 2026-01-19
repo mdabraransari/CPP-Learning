67516eed424c95a74777e3b4ada934e7bf569cce
Md Abrar Ansari <mdabraransari101@gmail.com>
2026-01-19 17:32:58 +0530

    constexpr and bool

---
+++ Practices/bool.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+bool accept() {
+      cout << "Do you want to proceed(y or n)?\n";
+      
+      char answer = 0;
+      cin >>  answer;
+
+    //   if(answer == 'y') return true;
+    //   return false;
+    switch (answer)
+    {
+    case 'y':
+        return true;
+        break;
+
+    case 'n':
+        return false;
+        break;
+    
+    default:
+        cout << "I'll take that for a no.\n";
+        return false;
+    }
+
+
+}
+
+bool accept2(){
+    int tries = 1;
+    while(tries < 4){
+        cout << "Do you want to proceed (y or n)?\n";
+        char answer = 0;
+        cin >> answer;
+
+        switch (answer)
+        {
+        case 'y':
+            return true;
+            break;
+        case 'n':
+            return false;
+        default:
+            cout << "I don't understand that:))\n";
+            ++tries;
+            break;
+        }
+        
+    }
+    cout << "I'll take that for a no.\n";
+    return false;
+}
+int main(){
+    accept2();
+}
---
+++ Practices/const.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+constexpr double  square(double x) {
+    return x * x;
+}
+
+int main() {
+
+   const int dmv = 17;
+   int var = 17;
+   cout << dmv << "\n";
+   constexpr double max1 = 1.4*square(dmv);
+   cout << max1;
+//    constexpr double max2 = 1.4*square(var); // error 
+   const double max3 = 1.4*square(var); // runtime
+
+
+   return 0;
+}
