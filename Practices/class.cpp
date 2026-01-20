4ee7734e70a2b3765bc5b46c6545cbc719bf9739
Md Abrar Ansari <mdabraransari101@gmail.com>
2026-01-20 23:33:33 +0530

    Class basic definition

---
+++ Practices/class.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class Vector
+{
+private:
+    int sz;
+    double& operator[](int i) { return elem[i]; }
+    int size() {return sz;}
+public:
+    double* elem;
+    Vector(int s):elem{new double[s]}, sz{s} {}
+};
+
+
+
+int main() {
+    Vector v(6);
+    for (int i = 0; i < 5; ++i) {
+        v.elem[i] = i * 2.5;
+    }
+    for (int i = 0; i < 5; ++i) {
+        cout << v.elem[i] << " ";
+    }
+    cout << endl;
+   return 0;
+}
