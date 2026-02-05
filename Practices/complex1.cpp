766484b1557232df847e8aa5eefe8f392aea5dc7
Md Abrar Ansari <mdabraransari101@gmail.com>
2026-02-05 23:43:28 +0530

    concrete classes arithmatic type

---
+++ Practices/complex1.cpp
@@ -0,0 +1,19 @@
+class complex {
+    double re, im;
+    public:
+        complex(double r, double i): re{r}, im{i} {} // construct complex for two scalars
+        complex(double r) : re{r}, im{0} {} // construct complex for one scalar
+        complex() : re{0}, im{0} {} //default complex
+
+
+        double real() const { return re;}
+        void real(double d) { re = d;}
+        double imag() const {return im;}
+        void imag(double d) { im = d;}
+
+        complex& operator+=(const complex& z) { re+=z.re, im+=z.im; return *this;}
+        complex& operator*=(complex);
+        complex& operator/=(complex);
+
+        complex operator+(complex a) {return complex(*this)+=a;}
+};
