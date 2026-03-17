77ec3a99ea3bf548fa60f5899fadfd2662f9f828
Md Abrar Ansari <mdabraransari101@gmail.com>
2026-03-17 18:05:23 +0530

    count_if algorithm

---
+++ Practices/count_if.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool isEven(int x) {
+    return x % 2 == 0;
+}
+
+int main(){
+    vector<int> v = {1,2,3,4,5,6,7,8};
+    int c = count_if(v.begin(),v.end(),isEven);
+
+    cout << "Even Numbers are " << c << "\n";
+
+    vector<pair<string,int>> k = {
+        {"A", 10},
+        {"B", 25},
+        {"C", 15}
+    };
+    int d = count_if(k.begin(),k.end(), [](pair<string,int> p){
+        return p.second > 12;
+    });
+    cout << d;
+
+}
