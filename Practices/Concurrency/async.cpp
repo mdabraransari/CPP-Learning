40bf8ef5bb80986911e7d949727b94120f429789
Md Abrar Ansari <mdabraransari101@gmail.com>
2026-04-02 20:58:26 +0530

    async function template

---
+++ Practices/Concurrency/async.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <future>
+#include <chrono>
+#include <thread>
+using namespace std;
+
+int add(int a, int b){
+    return a + b;
+}
+
+int main() {
+   
+   future<int> result = async(add, 5, 6);
+   cout <<  "Doing other work..\n";
+   this_thread::sleep_for(chrono::seconds(3));
+   cout << "Result = " << result.get();
+
+
+   return 0;
+}
