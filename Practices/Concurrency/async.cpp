91127096f2a7343b9f3ef60128dd811d184e549e
Md Abrar Ansari <mdabraransari101@gmail.com>
2026-04-03 23:15:00 +0530

    Type function

--- Practices/Concurrency/async.cpp
+++ Practices/Concurrency/async.cpp
@@ -4,17 +4,18 @@
 #include <thread>
 using namespace std;
 
-int add(int a, int b){
+int add(int a, int b)
+{
     return a + b;
 }
 
-int main() {
-   
-   future<int> result = async(add, 5, 6);
-   cout <<  "Doing other work..\n";
-   this_thread::sleep_for(chrono::seconds(3));
-   cout << "Result = " << result.get();
+int main()
+{
 
+    future<int> result = async(add, 5, 6);
+    cout << "Doing other work..\n";
+    this_thread::sleep_for(chrono::seconds(3));
+    cout << "Result = " << result.get();
 
-   return 0;
+    return 0;
 }
---
+++ Practices/Concurrency/type_function.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <limits>
+
+constexpr float min_value = std::numeric_limits<float>::min();
+constexpr int int_size = sizeof(int);
+
+int main()
+{
+    std::cout << "Minimum positive float value: " << min_value << '\n';
+    std::cout << "Size of int: " << int_size << " bytes\n";
+    return 0;
+}
