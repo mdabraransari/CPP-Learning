a87130987660d840bcc42a056122365ce5dd4ff8
Md Abrar Ansari <mdabraransari101@gmail.com>
2026-04-15 19:36:05 +0530

    Vector Arithmetic

---
+++ Practices/Concurrency/vector_arithmetic.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    vector<int> a{1,2,3};
+    vector<int> b{4,5,6};
+    vector<int> c(3);
+
+    for(int i = 0; i < a.size(); i++){
+        c[i] = a[i] + b[i];
+    }
+
+    for(int x: c)
+       cout << x << " ";
+}
