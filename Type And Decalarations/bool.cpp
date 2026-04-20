a18f287f5cdabc3d37bbd1a999cc50e23158c357
Md Abrar Ansari <mdabraransari101@gmail.com>
2026-04-20 19:01:31 +0530

    Boolean

---
+++ Type And Decalarations/bool.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    bool b1 = 7;
+    // bool b2 {7};
+    cout << b1 << endl;
+    // cout << b2 << endl;
+    int i = true;
+    cout << i << endl;
+    int i2{true};
+    cout << i2 << endl;
+
+
+
+   return 0;
+}
