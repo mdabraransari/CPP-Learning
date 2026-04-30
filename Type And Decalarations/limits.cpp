06f5f3aaa479121f86d6ff1cbd38704e73ed2c24
Md Abrar Ansari <mdabraransari101@gmail.com>
2026-04-30 17:48:29 +0530

    limits

---
+++ Type And Decalarations/limits.cpp
@@ -0,0 +1,16 @@
+#include <limits>
+#include <iostream>
+#include <type_traits>
+using namespace std;
+
+int main(){
+    cout << boolalpha;
+
+    cout << "size of long " << sizeof(1L)<< '\n';
+    cout << "size of long long " << sizeof(1LL) << '\n';
+
+    cout << "largest float == "<< numeric_limits<float>::max() << '\n';
+    cout << "char is signed == "<< numeric_limits<char>::is_signed<< '\n';
+    cout << "char is unsigned == "<< is_unsigned<char>::value << '\n';
+
+}
