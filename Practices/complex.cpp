4daf253bf388369b5aaed1cd47ade2f9738a44b5
Md Abrar Ansari <mdabraransari101@gmail.com>
2026-05-10 22:37:38 +0530

    file modified

--- Practices/complex.cpp
+++ Practices/complex.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main() {
 
    complex<double> z(1, 4);
+   cout << "Complex Number:\n";
    cout << "Real: " << z.real() << endl;
    cout << "Imaginary: " << z.imag() << endl;
 
