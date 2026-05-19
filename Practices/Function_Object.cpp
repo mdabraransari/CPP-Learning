f32e97a2ee013516d37a242939182d1562f35c0b
Md Abrar Ansari <mdabraransari101@gmail.com>
2026-05-19 23:11:34 +0530

    Arithmetic operations by classes:(Exams are over now, and I’m moving into my second year.)

--- Practices/Function_Object.cpp
+++ Practices/Function_Object.cpp
@@ -3,16 +3,42 @@ using namespace std;
 
 class Add
 {
-    public:
-        int operator()(int a, int b)
+public:
+    int operator()(int a, int b, char c)
+    {
+        switch (c)
         {
+        case '+':
             return a + b;
+            break;
+        case '-':
+            return a - b;
+            break;
+        case '*':
+            return a * b;
+            break;
+        case '/':
+            return a / b;
+            break;
+        default: 
+            return -1;
+            break;
         }
+    }
 };
 
-int main() {
+int main()
+{
 
-   Add obj;
-   cout << obj(2,3);
-   return 0;
+    Add obj;
+    int a, b;
+    char c;
+    cout << "Enter first Number:\n";
+    cin >> a;
+    cout << "Enter second  Number:\n";
+    cin >> b;
+    cout << "Enter operation:\n";
+    cin >> c;
+    cout << "The given operation for " << a << " and " << b << " is " << obj(a, b, c);
+    return 0;
 }
