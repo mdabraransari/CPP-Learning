0623182187946574da6b99255c62ae21e1b88480
Md Abrar Ansari <mdabraransari101@gmail.com>
2026-05-24 22:28:59 +0530

    Lifetime of Objects

---
+++ Type And Decalarations/life_time_of_Objects.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+class Object{
+    public:
+       Object(const string& name){
+        this->name = name;
+        cout << "Constructor called for " << name << endl;
+
+       }
+       ~Object(){
+        cout << "Destructor called for " << name << endl;
+       }
+       private:
+         string name;
+};
+
+Object globalObj("Global Object");
+
+void functionExample(){
+    Object localObj("Local Object");
+    static Object staticObj("Static Object");
+
+    Object* dynamicObj = new Object("Dynamic Object");
+
+    cout << "Instide functionExample()" << endl;
+
+    delete dynamicObj;
+}
+
+int main(){
+    cout << "Program started\n" << endl;
+    functionExample();
+    cout <<"Calling function again...\n" << endl;
+    functionExample();
+    cout << "Program ending" << endl;
+    return 0;
+}
