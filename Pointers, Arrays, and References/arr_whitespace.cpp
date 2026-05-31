c4241e420c13fa164a04dcb4d91e2a41c37cb95f
Md Abrar Ansari <mdabraransari101@gmail.com>
2026-05-31 17:15:46 +0530

    Long strings can be broken by whitespaces to make the program text neater. The compiler will concatenate adjacenate strings

---
+++ Pointers, Arrays, and References/arr_whitespace.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+   char alpha[] = "abcdefghijklmnopqrstuvwxyz"
+                   "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    cout << alpha;
+   return 0;
+}
