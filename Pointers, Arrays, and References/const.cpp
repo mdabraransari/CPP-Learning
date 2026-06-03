d1e4852fd42b5128f6a38286274ba78b22dedfbd
Md Abrar Ansari <mdabraransari101@gmail.com>
2026-06-03 12:57:43 +0530

    fixed the error in  file

--- Pointers, Arrays, and References/const.cpp
+++ Pointers, Arrays, and References/const.cpp
@@ -2,8 +2,8 @@ const int model = 9;
 const int v[] = {1,2,3,4};
 const int x;
 void f(){
-    model = 200;//error
-    v[2] = 3; // error
+    // model = 200;//error
+    // v[2] = 3; // error
 }
 void g(const X* p){
     //can't modify *p here
