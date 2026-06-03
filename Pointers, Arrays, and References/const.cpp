074b887612fbcab0d9fec54f050bcc3d08f4b496
Md Abrar Ansari <mdabraransari101@gmail.com>
2026-06-03 12:55:00 +0530

    Two related meanings of "constant" >> constexpr: Evaluate at compile time >> const: Do not modify in this scope

---
+++ Pointers, Arrays, and References/const.cpp
@@ -0,0 +1,18 @@
+const int model = 9;
+const int v[] = {1,2,3,4};
+const int x;
+void f(){
+    model = 200;//error
+    v[2] = 3; // error
+}
+void g(const X* p){
+    //can't modify *p here
+}
+
+struct X{
+
+};
+void h(){
+    X val;
+    g(&val);
+}
