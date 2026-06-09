1640ae3773a6af10c2d7025052163359dfaf40de
Md Abrar Ansari <mdabraransari101@gmail.com>
2026-06-09 09:38:49 +0530

    Pair sum with O(n)

--- Questions/Apna College/pair_sum.cpp
+++ Questions/Apna College/pair_sum.cpp
@@ -6,18 +6,33 @@ vector<int> pair_Sum(vector<int> nums, int target)
 {
     int n = nums.size();
     vector<int> ans;
-    for (int i = 0; i < n; i++)
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = i + 1; j < n; j++)
+    //     {
+    //         if ((nums[i] + nums[j]) == target)
+    //         {
+    //             ans.push_back(i);
+    //             ans.push_back(j);
+    //               return ans;
+    //         }
+    //     }
+    // }
+    int i = 0, j = n -1;
+    while (i < j)
     {
-        for (int j = i + 1; j < n; j++)
-        {
-            if ((nums[i] + nums[j]) == target)
-            {
-                ans.push_back(i);
-                ans.push_back(j);
-                  return ans;
-            }
+        int pairSum = nums[i] + nums[j];
+        if(pairSum > target){
+            j--;
+        } else if(pairSum < target){
+            i++; 
+        } else {
+            ans.push_back(i);
+            ans.push_back(j);
+            return ans;
         }
     }
+    
     return {};
    
 }
@@ -26,7 +41,7 @@ int main()
 
     vector<int> nums = {2, 7, 11, 15};
 
-    int target = 9;
+    int target = 26;
     vector<int> ans =  pair_Sum(nums, target);
     cout << ans[0] << ", " << ans[1] << endl;
 
