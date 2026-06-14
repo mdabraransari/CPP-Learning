526cae53ce0187607e1e07896bddd1da43478397
Md Abrar Ansari <mdabraransari101@gmail.com>
2026-06-14 16:44:46 +0530

    pointer arithmetic add or subtract the bytes

---
+++ Practices/arr_arithmetic.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5};
+    int a = 10;
+    int* ptr = &a;
+
+    cout << ptr << endl;
+    ptr++;
+    cout << ptr << endl;
+    ptr++;
+    cout << ptr << endl;
+    return 0;
+}
