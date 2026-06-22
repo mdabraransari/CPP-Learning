c3a65acd881be49fec332d5a5c2d8048613e1dfd
Md Abrar Ansari <mdabraransari101@gmail.com>
2026-06-22 14:15:49 +0530

    stack

---
+++ Practices/stack.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+    stack<int> s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    stack<int> s2;
+    s2.swap(s);
+    // while (!s.empty())
+    // {
+    //     cout << s.top() << " ";
+    //     s.pop();
+    // }
+    cout << s.size() << endl;
+    cout << s2.size() << endl;
+
+    
+   return 0;
+}
