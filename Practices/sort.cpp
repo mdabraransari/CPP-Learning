6cc0747970b084409c9b223477bf7bd4b444b7ba
Md Abrar Ansari <mdabraransari101@gmail.com>
2026-06-24 15:04:39 +0530

    pair got sorted by its first element

--- Practices/sort.cpp
+++ Practices/sort.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 using namespace std;
 
 int main(){
     int arr[5] = {5, 4, 3, 2, 1};
     sort(arr, arr + 5 );
     sort(arr,arr + 5, greater<int>());
+    vector<pair<int, int>> vec = { {3,2},{2,1},{7,1},{5,2}};
+    sort(vec.begin(), vec.end());
 
-    for(int val: arr){
-        cout << val << " ";
+    // for(int val: arr){
+    //     cout << val << " ";
+    // }
+    // cout << endl;
+    for(auto p: vec){
+        cout << p.first << " " << p.second << endl;
     }
-    cout << endl;
     return 0;
 }
