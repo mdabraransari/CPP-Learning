90b0757da04aa5f13130ac5b2c6a316086469bc0
Md Abrar Ansari <mdabraransari101@gmail.com>
2026-06-28 20:50:37 +0530

    git commit

--- Questions/Apna College/reverse_string.cpp
+++ Questions/Apna College/reverse_string.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include <string>
-#
-using namespace std;
-string reverse_string(string str){
+#include <vector>
+#include <algorithm>
 
+using namespace std;
+string reverse_string(vector<string>& str){
+
+    
+           
 }
 int main() {
     string s;
