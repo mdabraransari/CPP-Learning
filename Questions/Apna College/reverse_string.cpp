c63c98576850450a750edbcf178dd43a79865769
Md Abrar Ansari <mdabraransari101@gmail.com>
2026-06-29 22:33:48 +0530

    added lines to code

--- Questions/Apna College/reverse_string.cpp
+++ Questions/Apna College/reverse_string.cpp
@@ -2,19 +2,37 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <sstream>
 
 using namespace std;
-string reverse_string(vector<string>& str){
+string reverse_string(const string& str) {
+    istringstream input(str);
+    vector<string> words;
+    string word;
 
-    
-           
+    while (input >> word) {
+        words.push_back(word);
+    }
+
+    reverse(words.begin(), words.end());
+
+    string result;
+    for (int i = 0; i < words.size(); i++) {
+        if (i > 0) {
+            result += " ";
+        }
+        result += words[i];
+    }
+
+    return result;
 }
+
 int main() {
     string s;
 
     cout << "Enter a string: ";
     getline(cin, s);
-
+    string reversed = reverse_string(s);
     /*
         Problem:
         Reverse the order of words in the string.
@@ -31,7 +49,7 @@ int main() {
 
     // Write your logic here.
 
-    cout << "Reversed words: " << endl;
+    cout << "Reversed words: " << reversed << endl;
 
     return 0;
 }
