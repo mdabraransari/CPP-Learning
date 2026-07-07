804d20a5c85bd7e1f7b1383aec6b15de2d426b8e
Md Abrar Ansari <mdabraransari101@gmail.com>
2026-07-07 15:14:41 +0530

    file added

--- Bjarne Stroustrup's Book/4. Structures, Unions, and Enumerations/enum_classes.cpp
+++ Bjarne Stroustrup's Book/4. Structures, Unions, and Enumerations/enum_classes.cpp
@@ -1,6 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
 enum class Traffic_light{red, yellow , green};
 enum class Warning {green, yellow , orange, red};
 Warning a1 = 7;//error: no int->Warning conversion
 int a2 = green; // error: green not in scope;
 int a3 = Warning::green;// error: no Warning -> int conversion
 Warning a4 = Warning::green; //ok
+}
