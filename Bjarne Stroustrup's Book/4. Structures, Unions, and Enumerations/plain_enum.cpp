e4e0998f490d825378963db5212dc30d451f54e0
Md Abrar Ansari <mdabraransari101@gmail.com>
2026-07-10 21:12:26 +0530

    10 july

--- Bjarne Stroustrup's Book/4. Structures, Unions, and Enumerations/plain_enum.cpp
+++ Bjarne Stroustrup's Book/4. Structures, Unions, and Enumerations/plain_enum.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+int main(){
 enum Traffic_light { red, yellow, green };
 enum Warning { green, yellow, orange, red }; // fire alert levels
 // error : two definitions of yellow (to the same value)
@@ -6,3 +8,4 @@ Warning a1 = 7; // error : no int->War ning conversion
 int a2 = green; // OK: green is in scope and converts to int
 int a3 = Warning::green; // OK: War ning->int conversion
 Warning a4 = Warning::green;
+}
