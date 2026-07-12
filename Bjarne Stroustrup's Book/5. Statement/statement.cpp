890047faafe8ca874c3382bdd2c20484638bfd38
Md Abrar Ansari <mdabraransari101@gmail.com>
2026-07-12 23:14:26 +0530

    Expression becomes a statement when you add a semicolon at its end

---
+++ Bjarne Stroustrup's Book/5. Statement/statement.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+//Declaration Statement
+int age;
+float salary;
+// Assignment Statement
+age = 20;
+salary = 50000.5;
+//Expression Statement
+age + salary;
+age++;
+int sum = age + salary;
+// Input Statement
+cin >> age;
+// Output Statement
+cout << "Age = " << age << endl;
+
+}
